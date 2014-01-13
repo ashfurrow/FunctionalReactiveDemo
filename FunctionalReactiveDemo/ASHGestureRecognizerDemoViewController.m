10d2a2d463458e99a668cc2ac195353c0933998b
Ash Furrow <ash@ashfurrow.com>
2014-01-13 16:30:49 -0500

    Finished gesture recognizer demo.

--- FunctionalReactiveDemo/ASHGestureRecognizerDemoViewController.m
+++ FunctionalReactiveDemo/ASHGestureRecognizerDemoViewController.m
@@ -8,7 +8,12 @@
 
 #import "ASHGestureRecognizerDemoViewController.h"
 
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
 @interface ASHGestureRecognizerDemoViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *boxView;
+
 @end
 
 @implementation ASHGestureRecognizerDemoViewController
@@ -19,6 +24,22 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIPanGestureRecognizer *recognizer = [[UIPanGestureRecognizer alloc] init];
+    [self.view addGestureRecognizer:recognizer];
+    
+    RAC(self.boxView, center) = [[recognizer rac_gestureSignal] map:^id(UIGestureRecognizer *recognizer) {
+        return [NSValue valueWithCGPoint:[recognizer locationInView:recognizer.view]];
+    }];
+    RAC(self.boxView, backgroundColor) = [[recognizer rac_gestureSignal] map:^id(id value) {
+        CGPoint location = [recognizer locationInView:recognizer.view];
+        CGFloat hue = location.x / CGRectGetWidth(recognizer.view.bounds);
+        CGFloat brightness = location.y / CGRectGetHeight(recognizer.view.bounds);
+        return [UIColor colorWithHue:hue saturation:1.0f brightness:brightness alpha:1.0f];
+    }];
+    [[recognizer rac_gestureSignal] subscribeNext:^(UIPanGestureRecognizer *recognizer) {
+        CGPoint location = [recognizer locationInView:recognizer.view];
+        NSLog(@"(%f, %f)", location.x, location.y);
+    }];
 }
 
 @end
