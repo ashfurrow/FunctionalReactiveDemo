21d7e18ae2c377c0fe8f45495d0d4623f12e047a
Ash Furrow <ash@ashfurrow.com>
2014-01-13 16:50:28 -0500

    Added ASHLoginDemoViewController.

---
+++ FunctionalReactiveDemo/ASHLoginDemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  ASHLoginDemoViewController.h
+//  FunctionalReactiveDemo
+//
+//  Created by Ash Furrow on 1/13/2014.
+//  Copyright (c) 2014 Ash Furrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASHLoginDemoViewController : UIViewController
+
+@end
