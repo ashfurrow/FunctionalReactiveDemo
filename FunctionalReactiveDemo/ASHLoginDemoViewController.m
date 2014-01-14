e788fc8cddade292f032ec2547ac16299854a3dc
Ash Furrow <ash@ashfurrow.com>
2014-01-13 19:25:19 -0500

    Completed ASHLoginDemoViewController.

--- FunctionalReactiveDemo/ASHLoginDemoViewController.m
+++ FunctionalReactiveDemo/ASHLoginDemoViewController.m
@@ -10,29 +10,40 @@
 
 @interface ASHLoginDemoViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+
 @end
 
 @implementation ASHLoginDemoViewController
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    RACSignal *enabledSignal = [RACSignal combineLatest:@[self.emailTextField.rac_textSignal, self.passwordTextField.rac_textSignal]
+                                                 reduce:^id(NSString *emailAddress, NSString *password){
+                                                     BOOL emailValid = [emailAddress rangeOfString:@"@"].location != NSNotFound;
+                                                     BOOL passwordValid = password.length > 6;
+                                                     return @(emailValid && passwordValid);
+                                                 }];
+    
+    self.button.rac_command = [[RACCommand alloc] initWithEnabled:enabledSignal
+                                                      signalBlock:^RACSignal *(id input) {
+                                                          // Return a signal that represents a unit of work
+                                                          // It will be subscribed to when the button is pressed
+                                                          // Since this is confusing, it's being replaced with RACAction in ReactiveCocoa 3.0
+                                                          return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+                                                              NSLog(@"Button was pressed.");
+                                                              // Perform some long-running task, like a network operation
+                                                              [subscriber sendCompleted];
+                                                              
+                                                              // Return a RACDisposable representing a possible cancellation
+                                                              return nil;
+                                                          }];
+                                                      }];
 }
 
 @end
